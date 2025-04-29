d0d6816fdef8d0a28d93d0bb1146c9f4052c96fe
Jen <jensavino26@gmail.com>
2025-04-29 19:57:13 -0400

    support file false

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,12 +4,13 @@ export default defineConfig({
   component: {
     devServer: {
       framework: 'react',
-      bundler: 'vite'
-    }
+      bundler: 'vite',
+    },
+    supportFile: false, // Disable the support file for component testing
   },
   e2e: {
     baseUrl: 'http://localhost:3001',
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
-    supportFile: 'cypress/support/e2e.js'
+    supportFile: 'cypress/support/e2e.js',
   }
 })
