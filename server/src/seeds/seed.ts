eda1574f83049105f142d10518b0ee1c4ac5c24f
Jen <jensavino26@gmail.com>
2025-04-28 20:50:10 -0400

    working server and client. Good base to revert to

--- server/src/seeds/seed.ts
+++ server/src/seeds/seed.ts
@@ -2,7 +2,12 @@ import db from '../config/connection.js';
 import { Question } from '../models/index.js'
 import cleanDB from './cleanDb.js';
 
-import questionData from './pythonQuestions.json' assert{ type: 'json'};
+import fs from 'fs';
+import path from 'path';
+
+const questionData = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, './pythonQuestions.json'), 'utf-8')
+);
 
 try {
   await db();
