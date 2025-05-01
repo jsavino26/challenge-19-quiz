774841468cf11d4f7e5fc8a0822a60553653aa11
Jen <jensavino26@gmail.com>
2025-05-01 16:41:31 -0400

    Bug fixes

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,11 +6,11 @@ export default defineConfig({
       framework: 'react',
       bundler: 'vite',
     },
-    supportFile: false, // Disable the support file for component testing
+    
   },
   e2e: {
     baseUrl: 'http://localhost:3001',
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
-    supportFile: 'cypress/support/e2e.js',
+    supportFile: false,
   }
 })
--- cypress/e2e/quiz.cy.js
+++ cypress/e2e/quiz.cy.js
@@ -1,19 +1,52 @@
 describe('Tech Quiz E2E', () => {
+  const NUM_QUESTIONS = 20;
     beforeEach(() => {
+      cy.intercept('GET', '/api/questions/random', {
+        fixture: 'questions.json',
+      }).as('getQuestions');
       cy.visit('/');
     });
   
+    it('Starts a quiz', () => {
+      cy.contains('Start Quiz').click();
+        cy.get('h2').should('exist');
+        cy.get('button').should('exist'); // pick first answer
+  
+    });
+
     it('Starts and completes a quiz', () => {
       cy.contains('Start Quiz').click();
   
-      for (let i = 0; i < 10; i++) {
-        cy.get('.question').should('exist');
-        cy.get('.answer-button').first().click(); // pick first answer
+      for (let i = 0; i < NUM_QUESTIONS; i++) {
+        cy.get('h2').should('exist');
+        cy.get('button').first().click(); // pick first answer
       }
   
-      cy.contains('Your Score').should('exist');
-      cy.contains('Start New Quiz').click();
-      cy.contains('Start Quiz').should('exist');
+      cy.contains('Quiz Completed').should('exist');
+    });
+
+    it('Shows score at end of test', () => {
+      cy.contains('Start Quiz').click();
+  
+      for (let i = 0; i < NUM_QUESTIONS; i++) {
+        cy.get('h2').should('exist');
+        cy.get('button').first().click(); // pick first answer
+      }
+  
+      cy.contains('Your score').should('exist');
+    });
+
+    it('Start new quiz at end of test', () => {
+      cy.contains('Start Quiz').click();
+  
+      for (let i = 0; i < NUM_QUESTIONS; i++) {
+        cy.get('h2').should('exist');
+        cy.get('button').first().click(); // pick first answer
+      }
+  
+      cy.contains('Your score').should('exist');
+      cy.contains('Take New Quiz').click();
+      cy.get('h2').should('exist');
     });
   });
   
